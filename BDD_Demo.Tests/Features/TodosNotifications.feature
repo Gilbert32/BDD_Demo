795c7edd5f82e0f4dbf4f02ca9ae77bd19efb2d2
Gix <gilbert.jabbour@inmind.ai>
2021-02-23 20:02:51 +0100

    Add Tests Project

---
+++ BDD_Demo.Tests/Features/TodosNotifications.feature
@@ -0,0 +1,19 @@
+﻿Feature: Todos can have Notifications
+    As a User,
+    I want to have notifications for my todos
+
+Scenario: Notifications should appear for Todos that have reached their target dates
+    Given a new user is added to the database with the username aastha and the email aastha@bmw.de
+    When a todo is added to the user with the target date of 01.01.2021
+    Then if the user logs in he will have 1 notifications
+
+Scenario: Notifications should not appear for Todos that have reached their target dates and are complete
+    Given a new user is added to the database with the username jiawei and the email jiawei@bmw.de
+    When a todo is added to the user with the target date of 01.01.2021
+    And the todo is set to complete
+    Then if the user logs in he will have 0 notifications
+
+Scenario: Notifications should not appear for Todos that have not reached their target date
+    Given a new user is added to the database with the username jiawei and the email jiawei@bmw.de
+    When a todo is added to the user with the target date of 01.01.2022
+    Then if the user logs in he will have 0 notifications
---
+++ BDD_Demo.Tests/Steps/TodosNotificationsTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using BDD_Demo.Domain.Contexts;
+using BDD_Demo.Services;
+using BDD_Demo.Services.Requests;
+using BDD_Demo.Services.ViewModels;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace BDD_Demo.Tests.Steps
+{
+    [Binding]
+    public class TodosNotificationsTests
+    {
+        private InMemoryBddDemoContext _context;
+        private UserService _userService;
+        private TodoService _todoService;
+        private UserViewModel _user;
+        private TodoViewModel _todo;
+
+        public TodosNotificationsTests()
+        {
+            _context = new InMemoryBddDemoContext();
+            _userService = new UserService(_context);
+            _todoService = new TodoService(_context, _userService);
+        }
+
+
+        [When(@"a todo is added to the user with the target date of (.*)")]
+        public async void WhenATodoIsAddedToTheUserWithTheTargetDateOfX(string date)
+        {
+            _todo = await _todoService.AddTodo(new AddTodoRequest()
+                {UserId = _user.Id, Title = "test todo", TargetDate = DateTime.Parse(date), Text = "test todo body"});
+        }
+
+
+        [When(@"the todo is set to complete")]
+        public void WhenTheTodoIsSetToComplete()
+        {
+            _todoService.CompleteTodo(new CompleteTodoRequest() {Id = _todo.Id});
+        }
+
+        [Given(@"a new user is added to the database with the username (.*) and the email (.*)")]
+        public async void GivenANewUserIsAddedToTheDatabaseWithTheUsernameAndEmail(string username, string email)
+        {
+            _user = await _userService.AddUser(new AddUserRequest() {Email = email, UserName = username});
+        }
+
+        [Then(@"if the user logs in he will have (.*) notifications")]
+        public async void ThenIfTheUserLogsInHeWillHave0Notifications(string numberOfNotifications)
+        {
+            // First, get User again
+            var user = await _userService.GetUser(_user.Id);
+            user.Notifications.Count.Should().Be(int.Parse(numberOfNotifications));
+        }
+    }
+}
